51db69f9c8f33ca353935ab650b18f19ceaa18a3
Norishige Fukushima <norishigefukushima@users.noreply.github.com>
2019-05-26 19:34:54 +0900

    画像の追加

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@ for image in $1/test/*.ppm; do
 	echo `basename ${template}`
 	if [ $x = 0 ]
 	then
-	    ./matching $name "${template}" rotation 0.5 cpg
+	    ./matching $name "${template}" rotation 0.5 cp
 	    x=1
 	else
-	    ./matching $name "${template}" rotation 0.5 pg
+	    ./matching $name "${template}" rotation 0.5 p
 	fi
     done
     echo ""
